f2c64d0356082ade21f22eeaa61d5f28f6a67485
RITHISH <RITHISH@DESKTOP-EVCFLE1>
2020-01-26 18:49:42 +0530

    DUMMY CHANGE

--- tanqweb/src/app/app.component.ts
+++ tanqweb/src/app/app.component.ts
@@ -69,7 +69,7 @@ geolocation.on('change', function() {
   el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
   el('heading').innerText = geolocation.getHeading() + ' [rad]';
   el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
-  alert(geolocation.getPosition());
+ 
 });
 
 // handle geolocation error.
